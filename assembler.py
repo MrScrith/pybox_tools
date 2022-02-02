96a2ed4d1e0ec4e5f31b59287b215482de24b669
Erik <erik@trouserenthusiast.com>
2022-02-02 13:21:09 -0600

    Worked on function list, but going to stop to refactor.

--- assembler.py
+++ assembler.py
@@ -8,11 +8,12 @@ def main(argv):
     binaryFile = ''
     ifile = None
     bfile = None
+    userApp = True
 
-    helpMessage = "assemler.py - i <assemblyFileName> -o <binaryFileName>"
+    helpMessage = "assemler.py -i <assemblyFileName> -b <binaryFileName> -o"
 
     try:
-        options, args = getopt.getopt(argv, "hi:o:", ["asmfile=", "binfile="])
+        options, args = getopt.getopt(argv, "hi:b:o", ["asmfile=", "binfile="])
     except getopt.GetoptError:
         print(helpMessage)
         sys.exit(2)
@@ -22,8 +23,10 @@ def main(argv):
             print(helpMessage)
         elif opt in ("-i", "--asmfile"):
             inputFile = arg
-        elif opt in ("-o", "--binfile"):
+        elif opt in ("-b", "--binfile"):
             binaryFile = arg
+        elif opt in '-o':
+            userApp = False
 
     if inputFile == '':
         print(helpMessage)
@@ -53,10 +56,11 @@ def main(argv):
 
 
 def parseAsm(asmFile, binFile):
-    nextVariable = 0
-    progStrings = []
-    labelList = {}
-    variableList = {}
+    nextVariable = 1        # Index of variable from Stack, starts at 1 (stack 0 is end of var list)
+    progStrings = []        # List of program strings
+    labelList = {}          # List of labels for jump instructions
+    functionList = {0:{}}   # List of function variables
+    curFunc = 0             # Current function index
 
     SKIP_FOR_SET = "SKIPFORSET"
 
@@ -69,11 +73,16 @@ def parseAsm(asmFile, binFile):
 
         if line[0] == '@':
             # Variable (stack) value
-            vname = line.partition('@')[2]
-            if vname not in variableList.keys():
-                variableList[vname] = nextVariable
+
+            # extract the variable name (include the @), strip off spaces.
+            vname = line.partition('=')[0].strip()
+
+            # check to see if the name exists in the current function list
+            if vname not in functionList[curFunc].keys():
+                # Name does not exist, so add it to the list
+                functionList[curFunc][vname] = (nextVariable, line.partition('=')[2].strip())
                 nextVariable += 1
-            line = line.partition('@')[0] + '@' + variableList[vname]
+            line = line.partition('@')[0] + '@' + functionList[curFunc][vname]
 
         if '#' in line:
             # Comment exists within the line
@@ -96,6 +105,10 @@ def parseAsm(asmFile, binFile):
             if line.upper()[0:3] == "SET":
                 progStrings.append(SKIP_FOR_SET)
 
+        if line[0:3] == "FNC":
+            curFunc += 1
+            nextVariable = 0
+
     # Now that it's been pre-processed, now we start putting it all together.
     for line in progStrings:
 
@@ -110,47 +123,62 @@ def parseAsm(asmFile, binFile):
 
         parsed_com = line.upper().split(':')
 
-        comm = parsed_com[0][0:3]
+        comm = parsed_com[0]
 
-        if   comm == "CPY":
-            data = 0x0001 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "ST0":
-            data = 0x0000 | parse_dest(parsed_com[1])
-        elif comm == "ST1":
-            data = 0x1001 | parse_dest(parsed_com[1])
-        elif comm == "STN":
+        if   comm == "COPY":
+            data = 0x0000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "SET0":
+            data = 0x0800 | parse_dest(parsed_com[1])
+        elif comm == "SET1":
             data = 0x1000 | parse_dest(parsed_com[1])
-        elif comm == "INC":
-            data = 0x2001 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1])
-        elif comm == "DEC":
-            data = 0x2000 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1])
-        elif comm == "ADD":
-            data = 0x3001 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "SUB":
-            data = 0x3000 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "MUL":
-            data = 0x4001 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "DIV":
-            data = 0x4000 | parse_signed(parsed_com[0]) | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "AND":
-            data = 0x5001 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
-        elif comm == "ORR":
+        elif comm == "SETN":
+            data = 0x1800 | parse_dest(parsed_com[1])
+        elif comm == "UINC" or comm == "INC":
+            data = 0x2000 | parse_source(parsed_com[1])
+        elif comm == "SINC":
+            data = 0x2800 | parse_source(parsed_com[1])
+        elif comm == "UDEC" or comm == "DEC":
+            data = 0x3000 | parse_source(parsed_com[1])
+        elif comm == "SDEC":
+            data = 0x3800 | parse_source(parsed_com[1])
+        elif comm == "UADD" or comm == "ADD":
+            data = 0x4000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "SADD":
+            data = 0x4800 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "USUB" or comm == "SUB":
             data = 0x5000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "SSUB":
+            data = 0x5800 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "UMUL" or comm == "MUL":
+            data = 0x6000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "SMUL":
+            data = 0x6800 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "UDIV" or comm == "DIV":
+            data = 0x7000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "SDIV":
+            data = 0x7800 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "AND":
+            data = 0x8000 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
+        elif comm == "OR":
+            data = 0x8800 | parse_source(parsed_com[1]) | parse_dest(parsed_com[2])
         elif comm == "NOT":
-            data = 0x6001 | parse_source(parsed_com[1])
+            data = 0x9000 | parse_source(parsed_com[1])
         elif comm == "NEG":
-            data = 0x6000 | parse_source(parsed_com[1])
+            data = 0x9800 | parse_source(parsed_com[1])
         elif comm == "BSL":
-            data = 0x7001 | parse_source(parsed_com[1])
+            data = 0xA000 | parse_source(parsed_com[1])
         elif comm == "BSR":
-            data = 0x7000 | parse_source(parsed_com[1])
+            data = 0xA800 | parse_source(parsed_com[1])
         elif comm == "SET":
-            data = 0x8000 | parse_dest(parsed_com[1])
+            data = 0xB000 | parse_dest(parsed_com[1])
             hasLitValue = True
             val = parsed_com[2]
             if val[0] == '@':
                 # variable value...
                 # TODO figure out how to handle variables
+
+                # Check list of variables
+                #
                 pass
             elif val[0] == '(':
                 # value is label
@@ -161,26 +189,26 @@ def parseAsm(asmFile, binFile):
                 litData = int(val,0)
 
         elif comm == "FNC":
-            data = 0x9001
+            data = 0xB800
         elif comm == "FNR":
-            data = 0x9000
+            data = 0xC000
 
         if parsed_com[-1] == "NOJ":
             pass
         elif parsed_com[-1] == "JGZ":
-            data = data | 0x0002
+            data = data | 0x0200
         elif parsed_com[-1] == "JEZ":
-            data = data | 0x0004
+            data = data | 0x0400
         elif parsed_com[-1] == "JGE":
-            data = data | 0x0006
+            data = data | 0x0600
         elif parsed_com[-1] == "JLZ":
-            data = data | 0x0008
+            data = data | 0x0800
         elif parsed_com[-1] == "JLE":
-            data = data | 0x000A
+            data = data | 0x0A00
         elif parsed_com[-1] == "JNZ":
-            data = data | 0x000C
+            data = data | 0x0C00
         elif parsed_com[-1] == "JMP":
-            data = data | 0x000E
+            data = data | 0x0E00
 
         if binFile:
             binFile.write(struct.pack('H', data))
@@ -189,19 +217,6 @@ def parseAsm(asmFile, binFile):
                 binFile.write(struct.pack('H', litData))
 
 
-
-def  parse_signed(comm):
-
-    retVal = 0x0000
-
-    # If there is a signed designation on it
-    if len(comm) == 4:
-        # If the designation is "S" ("U" would be ignored/redundant)
-        if comm[3] == 'S':
-            retVal = 0x0010
-
-    return retVal
-
 def parse_source(source):
 
     rval = 0x0000
@@ -209,27 +224,35 @@ def parse_source(source):
     if source == "RJP":
         pass
     elif source == "RMA":
-        rval = 0x0100
+        rval = 0x0010
     elif source == "RMD":
-        rval = 0x0200
-    elif source == "RG1":
-        rval = 0x0300
-    elif source == "RG2":
-        rval = 0x0400
-    elif source == "RG3":
-        rval = 0x0500
-    elif source == "RG4":
-        rval = 0x0600
-    elif source == "RG5":
-        rval = 0x0700
+        rval = 0x0020
+    elif source == "GP1":
+        rval = 0x0030
+    elif source == "GP2":
+        rval = 0x0040
+    elif source == "GP3":
+        rval = 0x0050
+    elif source == "GP4":
+        rval = 0x0060
+    elif source == "GP5":
+        rval = 0x0070
+    elif source == "GP6":
+        rval = 0x0080
+    elif source == "GP7":
+        rval = 0x0090
+    elif source == "GP8":
+        rval = 0x00A0
     elif source == "RIS":
-        rval = 0x0800
+        rval = 0x00B0
     elif source == "RPC":
-        rval = 0x0900
-    elif source == "RST":
-        rval = 0x0A00
-    elif source == "RAL":
-        rval = 0x0B00
+        rval = 0x00C0
+    elif source == "STK":
+        rval = 0x00D0
+    elif source == "ALU":
+        rval = 0x00E0
+    elif source == "RFL":
+        rval = 0x0F0
 
     return rval
 
@@ -240,19 +263,25 @@ def parse_dest(dest):
     if dest == "RJP":
         pass
     elif dest == "RMA":
-        rval = 0x0020
+        rval = 0x0001
     elif dest == "RMD":
-        rval = 0x0040
-    elif dest == "RG1":
-        rval = 0x0060
-    elif dest == "RG2":
-        rval = 0x0080
-    elif dest == "RG3":
-        rval = 0x00A0
-    elif dest == "RG4":
-        rval = 0x00C0
-    elif dest == "RG5":
-        rval = 0x00E0
+        rval = 0x0002
+    elif dest == "GP1":
+        rval = 0x0003
+    elif dest == "GP2":
+        rval = 0x0004
+    elif dest == "GP3":
+        rval = 0x0005
+    elif dest == "GP4":
+        rval = 0x0006
+    elif dest == "GP5":
+        rval = 0x0008
+    elif dest == "GP6":
+        rval = 0x0009
+    elif dest == "GP7":
+        rval = 0x000A
+    elif dest == "GP8":
+        rval = 0x000B
 
     return rval
 
